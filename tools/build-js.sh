5b966485e397f1bf4089469838a661fee271f8fb
Dan Stewart <git@mail.danstewart.dev>
2022-04-11 21:22:19 +0100

    Impove setup process
    - Use hermit for managing node
    - Use npm for managing project
    - Add transpiled JS

---
+++ tools/build-js.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Transpiling javascript with swc to ./build..."
+
+if [[ $1 == "watch" ]]; then
+    npx swc \
+        --source-maps true \
+        --quiet \
+        --watch \
+        ./src/static/js/binder/ --out-dir ./build/ &
+else
+    npx swc \
+      --quiet \
+      ./src/static/js/binder/ --out-dir ./build/
+fi
