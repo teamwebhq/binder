5b816fc37734611633bc05504068388e4b65a50b
Dan Stewart <git@mail.danstewart.dev>
2022-04-22 16:29:18 +0100

    WIP: DynamicFrame enhancements

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/static/");
 
+    eleventyConfig.setUseGitIgnore(false);
+    eleventyConfig.ignores.add("README.md");
+    eleventyConfig.ignores.add("node_modules/**");
+    eleventyConfig.ignores.add("build/**");
+    eleventyConfig.ignores.add("dist/**");
+
     return {
         dir: {
             input: "src",
