6ece921d9eca0201dd8b1af4228e5999ab07f637
Dan Stewart <git@mail.danstewart.dev>
2022-11-14 10:05:36 +0000

    Add build/static

---
+++ build/static/js/binder/binder.js
@@ -0,0 +1,2 @@
+import{registerControllers}from"./register.js";import{Controller}from"./controller.js";import{html,css}from"./util.js";export{registerControllers,Controller,html,css}
+//# sourceMappingURL=binder.js.map
